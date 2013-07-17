df45befefc6e2c2633bdf08a0ab2003ef1a9aa26
Michael <asoi@mail.ru>
2013-07-17 14:13:22 +0000

    grader.js with --url option

--- grader.js
+++ grader.js
@@ -24,8 +24,20 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://warm-scrubland-2826.herokuapp.com/";
+
+var readURL = function(url) {
+  rest.get(url).on('complete', function(result, response){
+    if (result instanceof Error) {
+        console.error('Error: ' + util.format(response.message));
+    } else {
+        fs.writeFileSync(HTMLFILE_DEFAULT, result);
+    }
+  });
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,6 +77,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL to index.html', readURL, URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
